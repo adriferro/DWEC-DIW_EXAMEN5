6332c0595c6e5f1309f5b16ccd296e153808e06f
Adri <a.fernandezrodriguez2003@gmail.com>
2024-01-17 10:55:25 +0100

    Perfeccionamos el proyecto

--- scripts/index.js
+++ scripts/index.js
@@ -22,8 +22,8 @@ const validar = (e) => {
     nombre.value.trim().length === 0 && mensajesError.push("El nombre es un campo obligatorio"),
     nombre.style.outline = '1px solid red';
 
-    /*/^[a-zA-Z]*$/.test(nombre.value.trim()) && mensajesError.push("Un nombre propio comienza siempre por una letra mayúscula y no contiene números"),
-    nombre.style.outline = '1px solid red';*/
+    !/^[A-Z].{1}[a-zA-Z]*$/.test(nombre.value.trim()) && mensajesError.push("Un nombre propio comienza siempre por una letra mayúscula y no contiene números"),
+    nombre.style.outline = '1px solid red';
 
     !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(correo.value.trim()) && mensajesError.push("Introduce una dirección de correo electrónico válida"),
     correo.style.outline = '1px solid red';
@@ -59,6 +59,6 @@ const info = (e2) => {
     }
 }
 
-formulario.addEventListener('submit',info);
+formulario.addEventListener('',info);
 
 formulario.addEventListener('submit',validar);

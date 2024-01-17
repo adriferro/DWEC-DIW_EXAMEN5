18a6c9a09f34582a535f6336ea8a11dc8893895f
Adri <a.fernandezrodriguez2003@gmail.com>
2024-01-17 09:05:56 +0100

    Modificamos el js

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,17 @@
  * @github
  */
 
+const formulario = document.getElementById("formulario")
+const nombre = document.getElementById("nombre")
+const correo = document.getElementById("correo")
+const mensaje = document.getElementById("mensaje")
+const enviar = document.getElementById("enviar")
+const errores = document.getElementById("errores")
+
+const validar = (e) => {
+    e.preventDefault()
+    let mensajesError=[]
+
+    nombre.value.trim().length === 0 && mensajesError.push("El nombre es un campo obligatorio"),
+    nombre.style.outline = '1px solid red';
+}
